db427a4dad86539e9a4bae7d5199369183f91e15
isabeleeduardaoliveiraborges <161508846+isabeleeduardaoliveiraborges@users.noreply.github.com>
2025-11-14 17:22:19 -0300

    Add files via upload

--- bibliotec/banco_bibliotec.sql
+++ bibliotec/banco_bibliotec.sql
@@ -1,52 +1,54 @@
-CREATE TABLE USUARIO 
-( 
- id.usuario INT PRIMARY KEY, auto_incrment
- nome INT,  
- telefone INT,  
- email INT,  
- endereco INT,  
- cpf INT,  
-); 
+CREATE Database bibliotec;
+use bibliotec;
+
+CREATE TABLE USUARIO (
+    id_usuario INT PRIMARY KEY AUTO_INCREMENT,
+    nome VARCHAR(100),
+    telefone VARCHAR(20),
+    email VARCHAR(100),
+    endereco VARCHAR(200),
+    cpf VARCHAR(20)
+);
+
 
 CREATE TABLE AUTORES 
 ( 
- id_autor INT PRIMARY KEY, auto_incrment 
- nome INT,  
- nacionalidade INT,  
- biografia INT,  
- data_nacimento INT,  
+    id_autor INT PRIMARY KEY AUTO_INCREMENT,
+    nome VARCHAR(100),
+    nacionalidade VARCHAR(50),
+    biografia TEXT,
+    data_nascimento DATE
 ); 
 
 CREATE TABLE LIVROS 
 ( 
- id.livro INT PRIMARY KEY,auto_incrment  
- titulo INT,  
- genero INT,  
- ano_publicado INT,  
- editora INT,  
- numero _paginas INT,  
+    id_livro INT PRIMARY KEY AUTO_INCREMENT,
+    titulo VARCHAR(200),
+    genero VARCHAR(50),
+    ano_publicado INT,
+    editora VARCHAR(100),
+    numero_paginas INT
 ); 
 
 CREATE TABLE EMPRESTIMO 
 ( 
- id.emprestimo INT PRIMARY KEY, auto_incrment 
- data_emprestimo INT,  
- data_entrega_prevista INT,  
- id.usuario INT PRIMARY KEY, auto_incrment 
- id.livro INT PRIMARY KEY,auto_incrment  
+    id_emprestimo INT PRIMARY KEY AUTO_INCREMENT,
+    data_emprestimo DATE,
+    data_entrega_prevista DATE,
+    id_usuario INT,
+    id_livro INT,
+    FOREIGN KEY (id_usuario) REFERENCES USUARIO(id_usuario),
+    FOREIGN KEY (id_livro) REFERENCES LIVROS(id_livro)
 ); 
+
 
 CREATE TABLE ESCRITO 
 ( 
- id.edicao INT PRIMARY KEY,auto_incrment  
- nacionalidade INT,  
- multiplos_autores INT,  
- categoria INT,  
- id.livro INT PRIMARY KEY,auto_incrment  
- id_autor INT PRIMARY KEY,auto_incrment,  
+    id_edicao INT PRIMARY KEY AUTO_INCREMENT,
+    categoria VARCHAR(100),
+    multiplos_autores BOOLEAN,
+    id_livro INT,
+    id_autor INT,
+    FOREIGN KEY (id_livro) REFERENCES LIVROS(id_livro),
+    FOREIGN KEY (id_autor) REFERENCES AUTORES(id_autor)
 ); 
-
-ALTER TABLE EMPRESTIMO ADD FOREIGN KEY(id.usuario) REFERENCES USUARIO (id.usuario)
-ALTER TABLE EMPRESTIMO ADD FOREIGN KEY(id.livro) REFERENCES LIVROS (id.livro)
-ALTER TABLE ESCRITO ADD FOREIGN KEY(id.livro) REFERENCES LIVROS (id.livro)
-ALTER TABLE ESCRITO ADD FOREIGN KEY(id_autor) REFERENCES AUTORES (id_autor)
