3aa42a5be692233fe8bde0d3ddaad5f41122a63e
isabeleeduardaoliveiraborges <161508846+isabeleeduardaoliveiraborges@users.noreply.github.com>
2025-11-14 17:15:01 -0300

    Add files via upload

--- bibliotec/banco_bibliotec.sql
+++ bibliotec/banco_bibliotec.sql
@@ -1,52 +1,52 @@
-CREATE DATABASE bibliotec;
-USE bibliotec;
+CREATE TABLE USUARIO 
+( 
+ id.usuario INT PRIMARY KEY, auto_incrment
+ nome INT,  
+ telefone INT,  
+ email INT,  
+ endereco INT,  
+ cpf INT,  
+); 
 
--- Usuários
-CREATE TABLE usuarios (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    nome VARCHAR(100) NOT NULL,
-    email VARCHAR(150) UNIQUE NOT NULL,
-    senha VARCHAR(255) NOT NULL,
-    telefone VARCHAR(20),
-    localizacao VARCHAR(100),
-    biografia TEXT,
-    tipo ENUM('admin','usuario') DEFAULT 'usuario',
-    data_criacao TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-);
+CREATE TABLE AUTORES 
+( 
+ id_autor INT PRIMARY KEY, auto_incrment 
+ nome INT,  
+ nacionalidade INT,  
+ biografia INT,  
+ data_nacimento INT,  
+); 
 
--- Categorias
-CREATE TABLE categorias (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    nome VARCHAR(50) NOT NULL,
-    descricao TEXT,
-    cor_primaria VARCHAR(7),
-    cor_secundaria VARCHAR(7)
-);
+CREATE TABLE LIVROS 
+( 
+ id.livro INT PRIMARY KEY,auto_incrment  
+ titulo INT,  
+ genero INT,  
+ ano_publicado INT,  
+ editora INT,  
+ numero _paginas INT,  
+); 
 
--- Livros
-CREATE TABLE livros (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    titulo VARCHAR(200) NOT NULL,
-    autor VARCHAR(100) NOT NULL,
-    descricao TEXT,
-    imagem_capa VARCHAR(255),
-    categoria_id INT,
-    preco DECIMAL(10,2),
-    ano_publicacao INT,
-    FOREIGN KEY (categoria_id) REFERENCES categorias(id)
-);
+CREATE TABLE EMPRESTIMO 
+( 
+ id.emprestimo INT PRIMARY KEY, auto_incrment 
+ data_emprestimo INT,  
+ data_entrega_prevista INT,  
+ id.usuario INT PRIMARY KEY, auto_incrment 
+ id.livro INT PRIMARY KEY,auto_incrment  
+); 
 
--- Livros lidos
-CREATE TABLE livros_lidos (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    usuario_id INT,
-    livro_id INT,
-    data_leitura TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    avaliacao INT CHECK (avaliacao >= 1 AND avaliacao <= 5),
-    FOREIGN KEY (usuario_id) REFERENCES usuarios(id),
-    FOREIGN KEY (livro_id) REFERENCES livros(id)
-);
+CREATE TABLE ESCRITO 
+( 
+ id.edicao INT PRIMARY KEY,auto_incrment  
+ nacionalidade INT,  
+ multiplos_autores INT,  
+ categoria INT,  
+ id.livro INT PRIMARY KEY,auto_incrment  
+ id_autor INT PRIMARY KEY,auto_incrment,  
+); 
 
--- Criar administrador
-INSERT INTO usuarios (nome, email, senha, tipo)
-VALUES ('Administrador', 'admin@bibliotec.com', MD5('1234'), 'admin');
+ALTER TABLE EMPRESTIMO ADD FOREIGN KEY(id.usuario) REFERENCES USUARIO (id.usuario)
+ALTER TABLE EMPRESTIMO ADD FOREIGN KEY(id.livro) REFERENCES LIVROS (id.livro)
+ALTER TABLE ESCRITO ADD FOREIGN KEY(id.livro) REFERENCES LIVROS (id.livro)
+ALTER TABLE ESCRITO ADD FOREIGN KEY(id_autor) REFERENCES AUTORES (id_autor)
